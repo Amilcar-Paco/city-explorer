a888b24bd3ba6f5b2fe437fc4f69c1769ef6bd53
Amílcar Paco <amilcarcpaco@gmail.com>
2024-06-18 12:21:38 +0200

    Remove unused code

--- city-explorer-frontend/src/index.tsx
+++ city-explorer-frontend/src/index.tsx
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store/store';
-import i18n from './i18n';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
