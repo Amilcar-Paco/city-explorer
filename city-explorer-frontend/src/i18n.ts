1c04595c0fe04b63818a9c2695616cc112746a45
Amílcar Paco <amilcarcpaco@gmail.com>
2024-06-18 12:20:36 +0200

    Initialize i18n configuration and set up language detection

---
+++ city-explorer-frontend/src/i18n.ts
@@ -0,0 +1,35 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { en, pt } from "./locales";
+
+interface Resources {
+  [key: string]: {
+    translation: Record<string, string>;
+  };
+}
+
+const resources: Resources = {
+  en: {
+    translation: en,
+  },
+  pt: {
+    translation: pt,
+  },
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: process.env.REACT_APP_DEFAULT_LANGUAGE || "en", // Default language fallback
+    interpolation: {
+      escapeValue: false,
+    },
+    react: {
+      useSuspense: false,
+    },
+  });
+
+export default i18n;
